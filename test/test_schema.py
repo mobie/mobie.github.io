0d5409d80f72a450cfa5a178b0094f9df38fe35d
Constantin Pape <c.pape@gmx.net>
2022-11-14 08:27:10 +0100

    Include example spec files in tests, adapt them to spec update

--- test/test_schema.py
+++ test/test_schema.py
@@ -5,17 +5,26 @@ import jsonschema
 
 
 class TestSchema(unittest.TestCase):
-    schema_root = os.path.join(
-        os.path.split(__file__)[0], "../schema"
-    )
+    schema_root = os.path.join(os.path.split(__file__)[0], "../schema")
+    example_root = os.path.join(os.path.split(__file__)[0], "../specs/examples")
 
-    def _test_schema(self, schema_name):
+    def _test_schema(self, schema_name, examples=None):
         path = os.path.join(self.schema_root, schema_name + ".schema.json")
         self.assertTrue(os.path.exists(path))
 
         with open(path, "r") as f:
             schema = json.load(f)
         jsonschema.Draft7Validator.check_schema(schema)
+
+        if examples is not None:
+            for example in examples:
+                example_path = os.path.join(self.example_root, example)
+                assert os.path.exists(example_path)
+                with open(example_path) as f:
+                    spec = json.load(f)
+                for name, instance in spec.items():
+                    jsonschema.validate(instance=instance, schema=schema)
+
         return schema
 
     def test_dataset(self):
@@ -25,7 +34,8 @@ class TestSchema(unittest.TestCase):
         self._test_schema("project")
 
     def test_source(self):
-        schema = self._test_schema("source")
+        examples = ["image_source.json", "segmentation_source.json"]
+        schema = self._test_schema("source", examples)
 
         # test an example image source
         source = {"image": {"imageData": {"bdv.hdf5": {"relativePath": "blob.h5"}}}}
@@ -45,7 +55,8 @@ class TestSchema(unittest.TestCase):
         jsonschema.validate(instance=source, schema=schema)
 
     def test_view(self):
-        self._test_schema("view")
+        examples = ["single_source_view.json", "grid_view.json", "advanced_fig2c.json"]
+        self._test_schema("view", examples)
 
     def test_views(self):
         self._test_schema("views")
